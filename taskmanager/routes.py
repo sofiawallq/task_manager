eda42043876b56e98cf59115f8a8b7435c74e8b2
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-07-19 16:51:14 +0000

    add files for deployment

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -71,4 +71,12 @@ def edit_task(task_id):
         task.due_date = request.form.get("due_date")
         task.category_id = request.form.get("category_id")
         db.session.commit()
-    return render_template("edit_task.html", task=task, categories=categories)     
+    return render_template("edit_task.html", task=task, categories=categories)   
+  
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
